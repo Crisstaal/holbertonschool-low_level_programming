540fe0cdfc9b9901a5feffb4c1abfa1278578a42
Crisstaal <muyungacrys@gmail.com>
2023-09-28 12:32:59 -0700

    alot printf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - entry point
- *   * sizeof - size
- *    * Return: 0
- *     */
+ * main - entry point
+ * sizeof - size
+ * Return: 0
+ */
 
 int main(void)
 {
