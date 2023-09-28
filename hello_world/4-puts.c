d5e1e6918c90e07653fb8707955c880ee5dde268
Crisstaal <muyungacrys@gmail.com>
2023-09-28 11:25:14 -0700

    Program prints text

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Progarmming is like building a multibilingual puzzle");
+	return(0);
+}
