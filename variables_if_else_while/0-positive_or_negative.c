1254d370267e43b3b26932945a27c96f8688e6ce
Crisstaal <muyungacrys@gmail.com>
2023-09-29 12:14:59 -0700

    RANDOM NUMBER

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,10 @@
  */
 int main(void)
 {
-	int n;
+	int n = 0;
+
 	srand(time(0));
-	n - rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -22,7 +23,7 @@ int main(void)
 	}
 	else
 	{
-		printf("d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
