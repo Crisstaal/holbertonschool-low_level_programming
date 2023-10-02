9eb52229fc2f143c03cf0ffc7677a6cfde5afc8f
Crisstaal <muyungacrys@gmail.com>
2023-10-02 10:19:01 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strien - returns the length of string
+ * @s: string
+ * Return: length
+ */
+
+int _strien(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
