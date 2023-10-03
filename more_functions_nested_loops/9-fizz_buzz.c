94b8e26cbfa213732d77d784f0e3013eb73a20e7
Crisstaal <muyungacrys@gmail.com>
2023-10-03 13:05:54 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,21 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz\n");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz\n");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz\n");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d\n", i);
 		}
 	}
-	printf("\n");
 
 	return (0);
 }
