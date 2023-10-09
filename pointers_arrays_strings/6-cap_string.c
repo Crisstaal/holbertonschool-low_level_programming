600753f3edc9ca32636485afaf21aa68b937de8d
Crisstaal <muyungacrys@gmail.com>
2023-10-09 14:37:09 -0700

    capitalizes all words

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -30,8 +30,10 @@ char *cap_string(char *str)
 		str[index - 1] == '{' ||
 		str[index - 1] == '}' ||
 		index == 0)
-			str[index] -= 32;
-
+	if (islower(str[index]) && (index == 0 ||
+				!isalpha(str[index - 1]))){
+		str[index] = toupper(str[index]);
+	}
 		index++;
 	}
 
