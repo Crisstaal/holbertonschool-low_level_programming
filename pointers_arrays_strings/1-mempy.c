e18eada3a769eb39298990755a39765da55557a0
Crisstaal <muyungacrys@gmail.com>
2023-10-09 18:59:02 -0700

    copies memory area

---
+++ pointers_arrays_strings/1-mempy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: memory
+ * @src: meomry
+ * @n: number of bytes
+ *
+ * Return: copied memory
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int b = n;
+
+	for (i < n; a++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,21 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *custom_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char*src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *str);
+char *leet(char *c);
+char *memset(char *s, char b, unsigned int n);
+char *memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
