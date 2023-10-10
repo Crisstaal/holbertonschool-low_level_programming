8d96a3ae9025675f62000b23e9afbbd636b658b9
Crisstaal <muyungacrys@gmail.com>
2023-10-09 20:32:04 -0700

    prints the sum

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,12 +17,12 @@ void print_diagsums(int *a, int size)
 
 	for (k = 0; k < size; k++)
 	{
-		sum1 = sum1 + a[k *size + k];
+		sum1 = sum1 + a[k * size + k];
 	}
 
 	for (k = size - 1; k >= 0; k--)
 	{
-		sum2 += a[k *size + (size - k - 1)];
+		sum2 += a[k * size + (size - k - 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
