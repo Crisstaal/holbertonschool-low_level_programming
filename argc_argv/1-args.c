2fce7bc1f45c2552cdaea31c63232b1898196d47
Your Name <you@example.com>
2023-10-20 14:37:25 -0700

    prints

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	int numArgs = argc - 1;
+	int i = 1;
 	
 	printf("Number of arguments: %d\n", numArgs);
 	
-	for (int i = 1; i < argc; i++)
+	for (i < argc; i++)
 	{
 		printf("Argument %d: %s\n", i, argv[i]);
 	}
