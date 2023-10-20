6c89ed6b2b897cefc155d8cc3908b9bb6c09aedd
Your Name <you@example.com>
2023-10-20 15:18:09 -0700

    multiplications

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplications
+ * @argc - count
+ * @argv - argument
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 1)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a *b);
+
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
