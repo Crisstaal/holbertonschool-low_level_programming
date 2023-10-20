cc975677e49334f78b0abd27c07defcb80ec4877
Your Name <you@example.com>
2023-10-20 15:01:31 -0700

    prints

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - prints the number
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	(void) argv;
 	printf("%d\n", argc - 1);
 
 	return(0);
