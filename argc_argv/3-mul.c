1f3a53bcc876a26363a14621f8103c10a50d86ad
Your Name <you@example.com>
2023-10-20 21:57:51 -0700

    argc

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,16 +12,17 @@
 
 int main (int argc, char *argv[])
 {
-	int a = 0, b = 0;
-
-	if (argc == 1)
+	if (argc == 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
 		printf("%d\n", a *b);
 
 		return (0);
 	}
+	else
+	{
 	printf("Error\n");
 	return (1);
+	}
 }
