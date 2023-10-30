c1fd3eb3893db6b66c6e8583aea3e1a9618e1c76
Your Name <you@example.com>
2023-10-29 23:09:57 -0700

    iterates

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "functions_pointers.h"
+
+/**
+ * array_iterator - iterate 
+ * @array: array
+ * @size: array size
+ * @action: function pointer
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		action(*array);
+		*array++;
+	}
+}
