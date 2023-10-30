2fa4df6c32e62cb38166ba17e986ac5a6a58b8c2
Your Name <you@example.com>
2023-10-29 22:10:01 -0700

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,8 +18,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	size_t n = strlen(name);
-	size_t o = strlen(owner);
+	n = strlen(name);
+	o = strlen(owner);
 
 	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
 	if (dog == NULL)
