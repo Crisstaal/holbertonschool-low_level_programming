88aa566f66e0883485aba2999815997547560f67
Your Name <you@example.com>
2023-10-29 20:36:58 -0700

    intialize

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * innit_dog - intialize variable
+ *
+ * @d: pointer
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
