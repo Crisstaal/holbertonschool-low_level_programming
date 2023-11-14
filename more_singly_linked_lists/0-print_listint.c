10e7338b7ed32c56d63f45fa8978d4a7e0fe1453
“github <you@example.com>
2023-11-13 20:51:05 -0800

    more singly

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,12 @@ size_t print_listint (const listint_t *h)
 	size_t num = 0;
 	while (h)
 	{
-		printf ("&d\n", h->n);
-		num++;
-		
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str;
 		h = h->next;
+		num++;
 	}
 		return (num);
 }
