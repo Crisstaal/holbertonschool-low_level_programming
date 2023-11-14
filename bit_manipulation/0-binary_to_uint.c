3d6ee1a0322919d35895bdcfbd7ada38142f4cdd
“github <you@example.com>
2023-11-13 21:34:05 -0800

    binary

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - converts to binary
@@ -10,25 +11,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, k, i, sum, pow;
-	int base;
-
-	base = 2;
-	sum = 0;
-	pow = 1;
+	unsigned int len;
+	int base = 2;
+	size_t k = 0;
+	size_t sum = 0;
+	size_t i = 0;
+	size_t pow = 1;
 
 	if (b == NULL)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 
-	if (len ==1 && (b[0] == '0' || b[0] == '1')
+	if (len == 1 && (b[0] == '0' || b[0] == '1'));
 			return (b[0] - 48);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 	if (b[i] != '0' && b[i] != '1')
 	return (0);
 
-	for (j = len - 1; j > 0; j--)
+	for (k = len - 1; k > 0; k--)
 	pow = pow * base;
 	sum = sum + (pow * (b[i] - 48));
 	len--;
