f88c547bab725170f42a6f849db936b4e8edbd09
“github <you@example.com>
2023-11-13 21:06:57 -0800

    print

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * print listint - prints all the elements of a linked lists
- * @ch: linked list of type listint_t to print
+ * print_listint - prints all the elements of a linked lists
+ * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-       if(h == NULL)
+	if(h == NULL)
 	       return (0);
-	for (num = 0; h != NULL; num ++)
+	for (num = 0; h != NULL; num++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-
 	}
 		return (num);
 }
