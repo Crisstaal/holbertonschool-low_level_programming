00fd8f593d3fb7688b329145aa250a98ddec9178
“github <you@example.com>
2023-11-13 20:53:43 -0800

    more singly

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -5,15 +5,15 @@
  * Return: number of nodes
  */
 
-size_t print_listint (const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t s = 0;
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		num++;
 	}
