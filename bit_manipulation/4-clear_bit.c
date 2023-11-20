cc9f11b46d8b130dba32f5e963f48885992daff2
“github <you@example.com>
2023-11-20 00:41:29 -0800

    sets the value of a bit to 0

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: pointer
+ * @index: starting from 0 of the bit you want to set
+ * Return: integer
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int a;
+
+	a = 1;
+	a = a << index;
+	if (index > sizeof(unsigned long int) * 8 && a == NULL)
+		return (-1);
+
+	if (((*n) >> index & 1) = 1)
+		*n = a ^ *n;
+
+	return (1);
+}
