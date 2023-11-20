144c0cc1bae4065e3de5b2abbb822abd3eadcaf3
“github <you@example.com>
2023-11-20 02:11:10 -0800

    appends text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	if (text_content != NULL)
 	{
-		for (letters = 0; text_content[letters]; letters++;);
+		for (letters = 0; text_content[letters]; letters++);
 
 		rwr = write(filedesc, text_content, letters);
 	}
