45a9fc2451fce0787746b7e6dd3990070cde4d92
“github <you@example.com>
2023-11-19 23:51:24 -0800

    converts a binary number

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,6 @@
  * binary_to_uint - converts to binary number to an unsigned int
  *
  * @b: character pointer
- *
  * Return: the converted number or 0
  */
 
@@ -23,8 +22,11 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++);
 
+
 	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+	{
 			return (b[0] - 48);
+	}
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
