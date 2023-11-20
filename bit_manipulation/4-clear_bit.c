72a9524622d0499a8fb5ace0c8a6d5a690bad75b
“github <you@example.com>
2023-11-20 00:43:54 -0800

    sets the value of a bit to 0

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	a = 1;
 	a = a << index;
-	if (index > sizeof(unsigned long int) * 8 && a == NULL)
+	if (index > sizeof(unsigned long int) * 8 || a == NULL)
 		return (-1);
 
 	if (((*n) >> index & 1) = 1)
