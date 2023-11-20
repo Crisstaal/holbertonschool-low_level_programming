d66fad1d8e05423079b38aeec2ce64cc3aaadeff
“github <you@example.com>
2023-11-20 00:55:34 -0800

    flip flip flip

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	answer = n ^ m;
 
-	for (count = 0; answer > 0)
+	for (count = 0; answer > 0;)
 	{
 		if ((answer & 1) == 1)
 			count++;
