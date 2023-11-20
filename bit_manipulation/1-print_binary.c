c37104153b21992a7a46b3a23c7bdf3424e66e8a
“github <you@example.com>
2023-11-20 00:17:59 -0800

    prints binary representation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,10 +4,6 @@
  * @n: decimal
  * Return: void
  */
-/**
- * print_binary - prints the binary
- * @n: decimal needed to convert
- */
 
 void print_bin(unsigned long int n)
 {
@@ -16,9 +12,14 @@ void print_bin(unsigned long int n)
 	print_bin(n >> 1);
 	if ((n & 1) == 1)
 		_putchar('1');
-	if ((n & 1 ) == 0)
+	if ((n & 1) == 0)
 		_putchar ('0');
 }
+/**
+ * print_binary - prints binary
+ * @n: decimal
+ */
+
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
